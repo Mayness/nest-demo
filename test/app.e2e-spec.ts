58f6b6e0c7cc98ff63ec338dfdb163fd176346cf
mayness <1095346833@qq.com>
2019-11-02 11:02:43 +0800

    test(app): 修改单测

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app;
-
-  beforeEach(async () => {
+  let token;
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const res = await request(app.getHttpServer())
+      .post('/api/login');
+    token = res.body.data;
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+
+  it('/cats/hello (GET)', async done => {
+    const res = await request(app.getHttpServer())
+            .get('/cats/hello')
+            .set('token', token);
+    expect(res.status).toBe(200);
+    expect(res.body.data).toEqual({ test: 1 });
+    done();
   });
 });
